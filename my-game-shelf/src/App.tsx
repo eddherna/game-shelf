89dfe126c3ff5274025787af3dca15bd24db7026
eduardo-hernandez-ueno <eduardo.hernandez@itti.digital>
2026-05-19 16:27:06 -0300

    remove game details panel and related styles

--- my-game-shelf/src/App.tsx
+++ my-game-shelf/src/App.tsx
@@ -2,7 +2,6 @@
 import React, {useEffect, useState} from "react";
 import "./App.css";
 import ShelfComponent from "./components/Shelf/shelf.component";
-import GameDetailsPanel from "./components/GameDetailsPanel/game-details-panel.component";
 import gameInfoService from "./service/game-info.service";
 import {SearchInputComponent} from "./molecules/SearchInput/search-input.component.tsx";
 
@@ -16,14 +15,13 @@ window.Buffer = window.Buffer || Buffer;
 const App: React.FC = () => {
     const [openGame, setOpenGame] = useState<number | null>(null);
     const [shelf, setShelf] = useState<Shelf>({gamesInfo: [], genres: new Set<string>()} as Shelf);
-    const selectedGame = shelf.gamesInfo.find((game) => game.id === openGame) ?? null;
     useEffect(() => {
         gameInfoService.getAll().then(setShelf);
     }, []);
 
 
     return (
-        <div className={`app ${selectedGame ? "details-open" : ""}`}>
+        <div className="app">
             <SearchInputComponent></SearchInputComponent>
 
             <GenreComboBoxComponent genres={shelf.genres} onGenresSelect={() => {}}></GenreComboBoxComponent>
@@ -31,11 +29,6 @@ const App: React.FC = () => {
                 games={shelf.gamesInfo}
                 openGame={openGame}
                 onSetOpenGame={setOpenGame}
-            />
-            <GameDetailsPanel
-                game={selectedGame}
-                isOpen={selectedGame !== null}
-                onClose={() => setOpenGame(null)}
             />
         </div>
     );
